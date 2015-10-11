c8c2892b0c4ff32b23c1dc19c75524660fad86fb
Frederick Jansen <frederick.jansen@gmail.com>
2015-10-10 21:21:04 -0400

    Partial file structure ready, just missing jasmine for testing

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name shufflingPines.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the shufflingPines
+ */
+angular.module('shufflingPines')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
