6be9d3b7fbbae9328fb23bddc14512cf4381548a
Frederick Jansen <frederick.jansen@gmail.com>
2015-10-25 20:47:11 -0400

    Fixed some JSHint issues

--- app/scripts/filters/capitalize.js
+++ app/scripts/filters/capitalize.js
@@ -1,11 +1,13 @@
-angular.module('shufflingPines.filters')
+'use strict';
 
+angular.module('shufflingPines.filters')
   // Capitalize first letter of string
-  .filter('capitalize', ['$filter',
-    function ($filter) {
-      return function (data) {
-        if (!data) return data;
-        return data.charAt(0).toUpperCase() + data.slice(1);
-      };
-    }
-  ]);
+  .filter('capitalize', function () {
+    return function (data) {
+      if (!data) {
+        return data;
+      }
+      return data.charAt(0).toUpperCase() + data.slice(1);
+    };
+  }
+);
