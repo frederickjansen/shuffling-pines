872db4af69689205c4022f0fd793dad9cb3a88dd
Frederick Jansen <frederick.jansen@gmail.com>
2015-11-04 20:28:51 -0500

    Fixed local storage spy and added all tests for guests service

--- test/spec/services/guests.js
+++ test/spec/services/guests.js
@@ -12,15 +12,17 @@ describe('Service: GuestService', function () {
   beforeEach(function () {
     var objStorage = {};
     localStorageService = {
-      get: function (key) {
-        return objStorage[key];
+      get: function () {
       },
-      set: function (key, value) {
-        objStorage[key] = value;
+      set: function () {
       }
     };
-    spyOn(localStorageService, 'get');
-    spyOn(localStorageService, 'set');
+    spyOn(localStorageService, 'get').and.callFake(function (key) {
+      return objStorage[key];
+    });
+    spyOn(localStorageService, 'set').and.callFake(function (key, value) {
+      objStorage[key] = value;
+    });
 
     module(function ($provide) {
       $provide.value('localStorageService', localStorageService);
@@ -63,8 +65,59 @@ describe('Service: GuestService', function () {
     expect(guests).toEqual(guestsObj);
   });
 
-  xit('', function () {
-
+  it('adding a guest should add one to storage', function () {
+    var guest = {
+      name: "FJ",
+      transitionDate: new Date(2015, 12, 20),
+      status: "pickup",
+      location: "123 St\r\nBoston"
+    };
+    GuestService.addGuest(guest);
+    var guestFromStorage = localStorageService.get('guests')[3];
+    expect(guestFromStorage).toBe(guest);
   });
-})
-;
+
+  it('deleted flag should be set to false on new guest', function () {
+    var guest = {
+      name: "FJ",
+      transitionDate: new Date(2015, 12, 20),
+      status: "pickup",
+      location: "123 St\r\nBoston",
+      deleted: true
+    };
+    GuestService.addGuest(guest);
+    expect(guest.deleted).toBe(false);
+  });
+
+  it('id should be +1 of existing guest count', function () {
+    var guest = {
+      id: 100,
+      name: "FJ",
+      transitionDate: new Date(2015, 12, 20),
+      status: "pickup",
+      location: "123 St\r\nBoston"
+    };
+    GuestService.addGuest(guest);
+    expect(guest.id).toBe(4);
+  });
+
+  it('removing guest should set deleted flag to true', function () {
+    GuestService.removeGuest(1);
+    var guests = GuestService.getGuests();
+    expect(guests[0].deleted).toBe(true);
+  });
+
+  it('editing a guest should edit it in storage', function () {
+    var guest = {
+      id: 1,
+      name: "FJ",
+      transitionDate: new Date(2015, 12, 20),
+      status: "pickup",
+      location: "123 St\r\nBoston",
+      deleted: false
+    };
+    GuestService.editGuest(1, guest);
+    var guests = GuestService.getGuests();
+    expect(guests[0].name).toBe('FJ');
+  });
+});
